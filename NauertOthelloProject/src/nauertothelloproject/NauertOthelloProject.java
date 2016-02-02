9337694645992b685f0fabcb8ea3d8cec91c3de1
LitoyN <litoynau@gmail.com>
2016-02-02 11:48:52 -0500

    Created new Netbeans project

---
+++ NauertOthelloProject/src/nauertothelloproject/NauertOthelloProject.java
@@ -0,0 +1,17 @@
+/*
+
+ */
+package nauertothelloproject;
+
+/**
+ *
+ * @author elliot
+ */
+public class NauertOthelloProject {
+
+    public constant 
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
