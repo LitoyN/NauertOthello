b190020bd7177e0efd16ba8af9a4c882aa7225ae
LitoyN <litoynau@gmail.com>
2016-02-03 23:25:04 -0500

    Started Implementing Basic Algorithm
    Main class and Board class

---
+++ NauertOthelloProject/src/nauertothelloproject/Board.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nauertothelloproject;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author elliot
+ */
+public class Board {
+    
+    int boardArray[];
+    int myScore;
+    int oppScore;
+    int noMovesCount;
+    boolean gameOver;
+    
+    public Board(String myColor){
+        boardArray = new int[100];
+        myScore = 2;
+        oppScore = 2;
+        gameOver = false;
+        noMovesCount = 0;
+        
+        Arrays.fill(boardArray,NauertOthelloProject.EMPTY);
+        for(int i = 0; i < 10; i++){
+            boardArray[i] = NauertOthelloProject.BORDER;
+            boardArray[90+i] = NauertOthelloProject.BORDER;
+            boardArray[i*10] = NauertOthelloProject.BORDER;
+            boardArray[i*10+9] = NauertOthelloProject.BORDER;
+        }
+        if(myColor == "b"){
+            boardArray[45] = NauertOthelloProject.ME;
+            boardArray[54] = NauertOthelloProject.ME;
+            boardArray[44] = NauertOthelloProject.OPPONENT;
+            boardArray[55] = NauertOthelloProject.OPPONENT;
+        }
+        else{
+            boardArray[45] = NauertOthelloProject.OPPONENT;
+            boardArray[54] = NauertOthelloProject.OPPONENT;
+            boardArray[44] = NauertOthelloProject.ME;
+            boardArray[55] = NauertOthelloProject.ME;
+
+        }
+    }
+    
+    public void getMoves(int player){
+        
+    }
+    
+    public void applyMove(int player, int move){
+        
+    }
+    
+    public boolean isGameOver(){
+        return gameOver;
+    }
+    
+    public void getScore(){
+        
+    }
+    
+}
--- NauertOthelloProject/src/nauertothelloproject/NauertOthelloProject.java
+++ NauertOthelloProject/src/nauertothelloproject/NauertOthelloProject.java
@@ -9,9 +9,34 @@ package nauertothelloproject;
  */
 public class NauertOthelloProject {
 
-    public constant 
+    public static final int EMPTY = 0;
+    public static final int ME = 1;
+    public static final int OPPONENT = -1;
+    public static final int BORDER = -2;
+    
+    static Board gameBoard;
+    static String myColor;
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int currentPlayer = 1;
+        boolean gameOver = false;
+        
+        myColor = getColor();
+        gameBoard = new Board("b");
+        
+        if(myColor == "b")
+            currentPlayer = ME;
+        else
+            currentPlayer = OPPONENT;
+        
+        while(!gameOver){
+            
+        }
+    }
+    
+    private static String getColor(){
+        String mycolor = "b";
+        return myColor;
     }
     
 }
