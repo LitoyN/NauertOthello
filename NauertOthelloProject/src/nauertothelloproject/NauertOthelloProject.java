4df45a394f021b168bff8085d1923a14b4ed68fc
LitoyN <litoynau@gmail.com>
2016-02-09 14:21:08 -0500

    Better formatting, UI move input (2)

--- NauertOthelloProject/src/nauertothelloproject/NauertOthelloProject.java
+++ NauertOthelloProject/src/nauertothelloproject/NauertOthelloProject.java
@@ -3,6 +3,8 @@
  */
 package nauertothelloproject;
 
+import java.util.Scanner;
+
 /**
  *
  * @author elliot
@@ -13,13 +15,17 @@ public class NauertOthelloProject {
     public static final int ME = 1;
     public static final int OPPONENT = -1;
     public static final int BORDER = -2;
+    public static final int[] DIRECTIONS = {-11,-10,-9,-1,1,9,10,11};
+    public static final int NUMDIRECTIONS = 8;
+    public static final String[] COLUMNS = {"A","B","C","D","E","F","G","H"};
     
     static Board gameBoard;
     static String myColor;
     
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
         int currentPlayer = 1;
-        boolean gameOver = false;
+        //boolean gameOver = false;
         
         myColor = getColor();
         gameBoard = new Board("b");
@@ -29,15 +35,35 @@ public class NauertOthelloProject {
         else
             currentPlayer = OPPONENT;
         
-        //while(!gameOver){
+        //while(!gameBoard.gameOver){
             
         //}
+        getMoveUI(keyboard);
         System.out.print(gameBoard.toString());
     }
     
     private static String getColor(){
         String mycolor = "b";
         return myColor;
+    }
+    
+    private static int getMoveUI(Scanner input){
+        int moveInt = 0;
+        int columnInt;
+        int rowInt;
+        int arrayPosition;
+        String moveString;
+        System.out.print("Please enter move: ");
+        moveString = input.next().toUpperCase();
+        columnInt = Character.getNumericValue(moveString.charAt(0)) - 9;
+        rowInt = Integer.parseInt(moveString.substring(1));
+        arrayPosition = 10*rowInt + columnInt;
+        System.out.println("Input string: " + moveString);
+        System.out.println("Row: " + rowInt);
+        System.out.println("Column: " + columnInt);
+        System.out.println("Array Position: " + arrayPosition);
+        
+        return moveInt;  
     }
     
 }
